bb9d839f1392c1c34f82f40c2c85b62002f648b6
itzJOHv <kaktusgame806@gmail.com>
2021-07-08 19:41:19 +0300

    Update update.js

--- src/commands/update.js
+++ src/commands/update.js
@@ -10,13 +10,15 @@ module.exports = {
 const { exec } = require('child_process');
 
 module.exports.run = async (message) => {
-    exec(`git pull origin main`, (error, stdout) => {
+    exec("git stash push --include-untracked");
+    exec("git pull origin main", (error, stdout) => {
+        exec("git stash drop");
     	let res = (error || stdout);
         if (error) return message.reply(error, { code: "fix" });
         if (res.includes("Already up to date.")) {
-            message.reply('Bot already up to date. No changes since last pull.');
+            message.reply("Bot already up to date. No changes since last pull.");
         } else {
-            message.reply('Pulled from GitHub. Restarting the bot.\n\nLogs:\n```' + res + '```')
+            message.reply("Pulled from GitHub. Restarting the bot.\n\nLogs:\n```" + res + "```")
                 .then(() => setTimeout(() => process.exit(), 1000));
         };
 	});
