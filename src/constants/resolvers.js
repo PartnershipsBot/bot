668cfe686ed781f573e31bfb642288b0d328bc03
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-07-20 01:59:04 +0300

    Update resolvers.js

--- src/constants/resolvers.js
+++ src/constants/resolvers.js
@@ -1,7 +1,6 @@
 module.exports.getChannel = (search, guild) => {
     const channels = guild.channels.cache.filter(ch => ch.type == "text" && ch.viewable);
     return false ||
-        channels.find(ch => search.toLowerCase() == ch.name.toLowerCase()) ||
         channels.get((search.match(idResolver) || ["0"])[0]);
 };
 
