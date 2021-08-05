794d6501aace99f2abffa587287d5c603ae4f01b
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-05 11:58:58 +0300

    f

--- src/commands/help.js
+++ src/commands/help.js
@@ -18,8 +18,8 @@ module.exports.run = async (message, args, gdb, { prefix, permissionLevel }) => 
         embed: {
             title: `Помощь ${client.user.username}`,
             description: [
-                `• Если Вам нужна помощь по команде, используйте \`${prefix}help <команда>\`.`,
-                "• Сервер поддержки: [discord.gg/sof](https://discord.gg/sof)"
+                `**\`•\`** Если Вам нужна помощь по команде, используйте \`${prefix}help <команда>\`.`,
+                "**`•`** Сервер поддержки: [discord.gg/sof](https://discord.gg/sof)"
             ].join("\n"),
             color: config.color,
             timestamp: Date.now(),
