911d9f733f35794625ba3adab2d7dfb254105b2e
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-08 19:43:30 +0300

    Update preview.js

--- src/commands/preview.js
+++ src/commands/preview.js
@@ -20,11 +20,11 @@ module.exports.run = async (message = new Message, args, gdb) => {
         owner = g.owner.user.tag.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)),
         ownerID = g.owner.user.id;
 
-    if (!description.length) return m.edit(`❌ Для начала опишите свой сервер используя команду \`${pref}description set\``);
-    if (!channel) return m.edit(`❌ Не удалось найти канал рассылки партнёрств на этом сервере. Вы указывали его используя команду \`${pref}channel set\`?`);
-    if (!channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return m.edit("❌ У меня нет прав на отправление сообщений в указанном канале.");
-    if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return m.edit("❌ У меня нет прав на создание приглашений в указанном канале.");
-    if (!invite) return m.edit("❌ Не удалось получить приглашение. Сообщите разработчику.");
+    if (!description.length) return message.reply(`❌ Для начала опишите свой сервер используя команду \`${pref}description set\``);
+    if (!channel) return message.reply(`❌ Не удалось найти канал рассылки партнёрств на этом сервере. Вы указывали его используя команду \`${pref}channel set\`?`);
+    if (!channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return message.reply("❌ У меня нет прав на отправление сообщений в указанном канале.");
+    if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.reply("❌ У меня нет прав на создание приглашений в указанном канале.");
+    if (!invite) return message.reply("❌ Не удалось получить приглашение. Сообщите разработчику.");
 
     let pv = new MessageEmbed()
         .setTitle(g.name)
