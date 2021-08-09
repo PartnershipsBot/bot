aedbe6706ec2f2f404a96f3091fe88021a71c3b5
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-09 20:39:28 +0300

    hhhhhhh

--- src/constants/resolvers.js
+++ src/constants/resolvers.js
@@ -4,19 +4,5 @@ module.exports.getInvite = async (guild) => {
     const channel = await guild.channels.resolve(guildDB.get().channel);
     if (!channel) return false;
 
-    let i;
-
-    guild.fetchInvites()
-        .then(invites => {
-            invites.find(invite => i = invite.inviter.id === client.user.id);
-        });
-
-    if (i) i = i.code;
-    else {
-        channel.createInvite({ maxAge: 0 })
-            .then(inv => {
-                i = inv.code;
-            });
-    };
-    return i;
+    return channel.createInvite({ maxAge: 0 }).then(inv => { return inv.code;});
 };
