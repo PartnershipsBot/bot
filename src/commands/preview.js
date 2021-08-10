1dca657d1c6a5d0de2ce247d8c2511054b042f96
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-10 19:01:07 +0300

    Update preview.js

--- src/commands/preview.js
+++ src/commands/preview.js
@@ -9,48 +9,91 @@ module.exports = {
 
 const { MessageEmbed, Message } = require("discord.js"), { color, prefix } = require("../../config"), { getInvite } = require("../constants/");
 
-module.exports.run = async (message = new Message, args, gdb) => {
-    const
-        g = message.guild,
-        description = gdb.get().description,
-        pref = gdb.get().prefix || prefix,
-        invite = await getInvite(g),
-        memberCount = g.members.cache.filter(member => !member.user.bot).size,
-        channel = g.channels.cache.get(gdb.get().channel),
-        owner = g.owner.user.tag.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)),
-        ownerID = g.owner.user.id;
+module.exports.run = async (message = new Message, args, gdb, { permissionLevel }) => {
+    if (permissionLevel => 3) {
+        const
+            g = client.guilds.cache.get(args[0]) || message.guild,
+            guildDB = await db.guild(g.id),
+            description = guildDB.get().description,
+            pref = guildDB.get().prefix || prefix,
+            invite = await getInvite(g),
+            memberCount = g.members.cache.filter(member => !member.user.bot).size,
+            channel = g.channels.cache.get(gdb.get().channel),
+            owner = g.owner.user.tag.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)),
+            ownerID = g.owner.user.id;
 
-    if (!description.length) return message.reply(`❌ Для начала опишите свой сервер используя команду \`${pref}description set\``);
-    if (!channel) return message.reply(`❌ Не удалось найти канал рассылки партнёрств на этом сервере. Вы указывали его используя команду \`${pref}channel set\`?`);
-    if (!channel.permissionsFor(message.guild.me).has("SEND_MESSAGES")) return message.reply("❌ У меня нет прав на отправление сообщений в указанном канале.");
-    if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.reply("❌ У меня нет прав на создание приглашений в указанном канале.");
-    if (!invite) return message.reply("❌ Не удалось получить приглашение. Сообщите разработчику.");
+        if (!channel) return message.reply(`❌ Не удалось найти канал рассылки партнёрств на этом сервере. Вы указывали его используя команду \`${pref}channel set\`?`);
+        if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.reply("❌ У меня нет прав на создание приглашений в указанном канале.");
+        if (!invite) return message.reply("❌ Не удалось получить приглашение. Сообщите разработчику.");
 
-    let pv = new MessageEmbed()
-        .setTitle(g.name)
-        .setThumbnail(g.iconURL({ dynamic: true, size: 64 }) || "https://cdn.discordapp.com/embed/avatars/0.png")
-        .setDescription(gdb.get().description)
-        .setFooter(`ID: ${gdb.get().guildid}`)
-        .setColor(gdb.get().color || color)
-        .setTimestamp()
-        .addFields([
-            {
-                name: "Участники",
-                value: memberCount,
-                inline: true
-            },
-            {
-                name: "Приглашение",
-                value: `[discord.gg/${invite}](https://discord.gg/${invite})`
-            },
-            {
-                name: "Владелец",
-                value: `\`${owner}\` (\`${ownerID}\`)`
-            }
-        ]);
-    if (g.banner) {
-        pv.setImage(g.bannerURL({ format: "png", size: 512 }));
-        pv.setThumbnail(g.iconURL({ dynamic: true, size: 128 }) || "https://cdn.discordapp.com/embed/avatars/0.png");
+        let pv = new MessageEmbed()
+            .setTitle(g.name)
+            .setThumbnail(g.iconURL({ dynamic: true, size: 64 }) || "https://cdn.discordapp.com/embed/avatars/0.png")
+            .setDescription(guildDB.get().description)
+            .setFooter(`ID: ${guildDB.get().guildid}`)
+            .setColor(guildDB.get().color || color)
+            .setTimestamp()
+            .addFields([
+                {
+                    name: "Участники",
+                    value: memberCount,
+                    inline: true
+                },
+                {
+                    name: "Приглашение",
+                    value: `[discord.gg/${invite}](https://discord.gg/${invite})`
+                },
+                {
+                    name: "Владелец",
+                    value: `\`${owner}\` (\`${ownerID}\`)`
+                }
+            ]);
+        if (g.banner) {
+            pv.setImage(g.bannerURL({ format: "png", size: 512 }));
+            pv.setThumbnail(g.iconURL({ dynamic: true, size: 128 }) || "https://cdn.discordapp.com/embed/avatars/0.png");
+        };
+    } else {
+        const
+            g = message.guild,
+            description = gdb.get().description,
+            pref = gdb.get().prefix || prefix,
+            invite = await getInvite(g),
+            memberCount = g.members.cache.filter(member => !member.user.bot).size,
+            channel = g.channels.cache.get(gdb.get().channel),
+            owner = g.owner.user.tag.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)),
+            ownerID = g.owner.user.id;
+
+        if (!description.length) return message.reply(`❌ Для начала опишите свой сервер используя команду \`${pref}description set\``);
+        if (!channel) return message.reply(`❌ Не удалось найти канал рассылки партнёрств на этом сервере. Вы указывали его используя команду \`${pref}channel set\`?`);
+        if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.reply("❌ У меня нет прав на создание приглашений в указанном канале.");
+        if (!invite) return message.reply("❌ Не удалось получить приглашение. Сообщите разработчику.");
+
+        let pv = new MessageEmbed()
+            .setTitle(g.name)
+            .setThumbnail(g.iconURL({ dynamic: true, size: 64 }) || "https://cdn.discordapp.com/embed/avatars/0.png")
+            .setDescription(gdb.get().description)
+            .setFooter(`ID: ${gdb.get().guildid}`)
+            .setColor(gdb.get().color || color)
+            .setTimestamp()
+            .addFields([
+                {
+                    name: "Участники",
+                    value: memberCount,
+                    inline: true
+                },
+                {
+                    name: "Приглашение",
+                    value: `[discord.gg/${invite}](https://discord.gg/${invite})`
+                },
+                {
+                    name: "Владелец",
+                    value: `\`${owner}\` (\`${ownerID}\`)`
+                }
+            ]);
+        if (g.banner) {
+            pv.setImage(g.bannerURL({ format: "png", size: 512 }));
+            pv.setThumbnail(g.iconURL({ dynamic: true, size: 128 }) || "https://cdn.discordapp.com/embed/avatars/0.png");
+        };
     };
 
     await message.reply(pv);
