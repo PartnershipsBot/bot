04f6e19c5c9f1649fd34988b488776e27a1dc723
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-13 00:38:36 +0300

    preload globals

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const config = require("../../config"), mongoose = require("mongoose");
 
-module.exports = () => {
-    mongoose.connect(config.database_uri, {
+module.exports = async () => {
+    await mongoose.connect(config.database_uri, {
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true,
@@ -10,6 +10,8 @@ module.exports = () => {
         log.error(e.message + ": " + e.stack);
         client.shard.send("respawn");
     });
+
+    await require("./global").load();
 
     return {
         guild: require("./guild")(), // guild(guildid)
