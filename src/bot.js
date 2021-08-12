2aa27728201d5f20993d3663bc30442c80143a44
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-13 00:47:29 +0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -72,8 +72,7 @@ client.on("message", async message => {
 
     const gdb = await db.guild(message.guild.id);
     global.gdb = gdb;
-    const gldb = await db.global();
-    global.gldb = gldb;
+    global.gldb = db.global;
 
     let { prefix } = gdb.get();
     if (!prefix.length) prefix = config.prefix;
