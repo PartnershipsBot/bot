80df69c1694e86694332da04c1f81651f6868f49
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-23 20:59:40 +0300

    Revert "ssss"
    This reverts commit f648abaf33ffc8c839256baef86a774f21d167ae.

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -2,9 +2,7 @@ const
     chalk = require("chalk"),
     Discord = require("discord.js"),
     { info, warn, error } = require("../../config").loggers,
-    e = "<a:fuck:868103266662232124>",
-    timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
-
+    e = "<a:fuck:868103266662232124>";
 const
     infoWH = new Discord.WebhookClient(info.id, info.token),
     warnWH = new Discord.WebhookClient(warn.id, warn.token),
@@ -20,7 +18,7 @@ module.exports = {
         warnWH.send(`[${timeFormatted} - INFO] ` + content);
     },
     error: (content) => {
-        console.log(chalk.whiteBright(`[${timeFormatted} - INFO] ` + content));
-        errorWH.send(`${e}<@419892040726347776>${e} [${timeFormatted} - INFO] ` + content);
+        console.log(chalk.redBright(`[${timeFormatted} - INFO] ` + content));
+        errorWH.send(`${e}<@419892040726347776>${e}[${timeFormatted} - INFO] ` + content);
     },
 };
