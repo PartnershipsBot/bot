f648abaf33ffc8c839256baef86a774f21d167ae
itzJOHv <68508885+itzJOHv@users.noreply.github.com>
2021-08-23 20:57:59 +0300

    ssss

--- src/handlers/logger.js
+++ src/handlers/logger.js
@@ -2,7 +2,9 @@ const
     chalk = require("chalk"),
     Discord = require("discord.js"),
     { info, warn, error } = require("../../config").loggers,
-    e = "<a:fuck:868103266662232124>";
+    e = "<a:fuck:868103266662232124>",
+    timeFormatted = new Date().toLocaleTimeString("ru-RU", { hour12: false });
+
 const
     infoWH = new Discord.WebhookClient(info.id, info.token),
     warnWH = new Discord.WebhookClient(warn.id, warn.token),
@@ -18,7 +20,7 @@ module.exports = {
         warnWH.send(`[${timeFormatted} - INFO] ` + content);
     },
     error: (content) => {
-        console.log(chalk.redBright(`[${timeFormatted} - INFO] ` + content));
-        errorWH.send(`${e}<@419892040726347776>${e}[${timeFormatted} - INFO] ` + content);
+        console.log(chalk.whiteBright(`[${timeFormatted} - INFO] ` + content));
+        errorWH.send(`${e}<@419892040726347776>${e} [${timeFormatted} - INFO] ` + content);
     },
 };
